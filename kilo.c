2e8c2b55f06eac9d70c132a7fbe1671671a85f15
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-05-28 21:57:41 -0300

    feat: add ICANON flag to allow turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -14,7 +14,7 @@ void enableRawMode()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_cflag &= ~(ECHO);
+    raw.c_cflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
