2234b88909aad89b0948714be9690f7640e8844f
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-05-29 10:48:39 -0300

    feat: turn off ctrl-c, ctrl-z and disable ctrl-s, ctrl-q

--- kilo.c
+++ kilo.c
@@ -16,7 +16,8 @@ void enableRawMode()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_cflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
