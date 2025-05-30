0dbf588496414209b2e8ed22efa03003ca644cef
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-05-30 15:49:56 -0300

    feat: disable ctrl-v and fix ctrl-m

--- kilo.c
+++ kilo.c
@@ -16,8 +16,8 @@ void enableRawMode()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(IXON);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_iflag &= ~(IXON | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
