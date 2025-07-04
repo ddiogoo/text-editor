cc70dad452fe98a09950cc21d8ded84efa45fb9f
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-03 23:57:58 -0300

    feat: Add pointer to row field to store multiple lines

--- kilo.c
+++ kilo.c
@@ -46,7 +46,7 @@ struct editorConfig
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -392,6 +392,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.row = NULL;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
